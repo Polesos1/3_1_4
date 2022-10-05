a8ab6c01961e28d7587803839e96c1c972239299
USER <gaverdov.pavel@gmail.com>
2022-10-05 14:18:22 +0300

    готово

---
+++ src/test/java/com/example/springbootrest/demo/SpringBootSecurityDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootrest.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSecurityDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
